ec6685cb02ddd17902f39b06250241d88c915430
Alex Klein <aklein53@gmail.com>
2020-05-06 11:08:50 -0400

    Updated workflows for new organization account. Fixed typo in hyperspectral example project name.

--- src/HyperspectralImageCapture/Program.cs
+++ src/HyperspectralImageCapture/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ChemImage.LCTF;
 
-namespace HyperspectralImageCpture
+namespace HyperspectralImageCapture
 {
 	class Program
 	{
--- src/HyperspectralImageCapture/Properties/AssemblyInfo.cs
+++ src/HyperspectralImageCapture/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HyperspectralImageCpture")]
+[assembly: AssemblyTitle("HyperspectralImageCapture")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HyperspectralImageCpture")]
+[assembly: AssemblyCompany("ChemImage")]
+[assembly: AssemblyProduct("HyperspectralImageCapture")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
