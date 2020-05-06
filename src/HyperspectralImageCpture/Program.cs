0310d3fa0a2bcf167bbb5550bfb6a158c4a74607
Alex Klein <aklein53@gmail.com>
2020-05-06 10:52:03 -0400

    Added a hyperspectral image capture example project. Updated ChemImageLCTF references to 1.2.1.

---
+++ src/HyperspectralImageCpture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChemImage.LCTF;
+
+namespace HyperspectralImageCpture
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Connecting to LCTF");
+			var lctf = LCTFController.GetFirstLCTF();
+
+			if (lctf == null)
+			{
+				Console.WriteLine("No LCTF available. Press any key to exit.");
+				Console.ReadKey();
+				return;
+			}
+
+			Console.WriteLine("Starting hyperspectral capture");
+			var min = lctf.WavelengthMin;
+			var max = lctf.WavelengthMax;
+			var step = lctf.WavelengthStep;
+			for (var currentWavelength = min; currentWavelength <= max; currentWavelength += step)
+			{
+				// We're in a non-async function, so we'll use Wait() instead of await
+				lctf.SetWavelengthAsync(currentWavelength).Wait();
+				Console.WriteLine($"Tuned to {currentWavelength} nm");
+
+				// Insert your image capture code here
+			}
+
+			// The lctf object needs to be disposed
+			lctf.Dispose();
+
+			Console.WriteLine("Hyperspectral capture completed. Press any key to exit.");
+			Console.ReadKey();
+			return;
+		}
+	}
+}
