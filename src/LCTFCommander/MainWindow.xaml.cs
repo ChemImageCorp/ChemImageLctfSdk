3d288cc92356a8ee996111e37419b0d8d06bcf42
Alex Klein <aklein53@gmail.com>
2020-05-11 14:31:41 -0400

    Enabled sequence cancelling. Tab/space cleanup.

--- src/LCTFCommander/MainWindow.xaml.cs
+++ src/LCTFCommander/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,232 +21,223 @@ using PropertyChanged;
 
 namespace LCTFCommander
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
-    {
-        public LCTFDeviceModel SelectedLCTF { get; set; } = null;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		public LCTFDeviceModel SelectedLCTF { get; set; } = null;
 
-        [DependsOn(nameof(SelectedLCTF))]
-        public int CurrentWavelength
-        {
-            get
-            {
-                return SelectedLCTF == null ? 0 : SelectedLCTF.CurrentWavelength;
-            }
-            set
-            {
-                if (SelectedLCTF != null)
-                {
-                    SelectedLCTF.CurrentWavelength = value;
-                }
-            }
-        }
+		[DependsOn(nameof(SelectedLCTF))]
+		public int CurrentWavelength
+		{
+			get
+			{
+				return SelectedLCTF == null ? 0 : SelectedLCTF.CurrentWavelength;
+			}
+			set
+			{
+				if (SelectedLCTF != null)
+				{
+					SelectedLCTF.CurrentWavelength = value;
+				}
+			}
+		}
 
-        [DependsOn(nameof(SelectedLCTF))]
-        public int WavelengthMin
-        {
-            get
-            {
-                return SelectedLCTF == null ? 0 : SelectedLCTF.LCTFDevice.WavelengthMin;
-            }
-        }
+		[DependsOn(nameof(SelectedLCTF))]
+		public int WavelengthMin
+		{
+			get
+			{
+				return SelectedLCTF == null ? 0 : SelectedLCTF.LCTFDevice.WavelengthMin;
+			}
+		}
 
-        [DependsOn(nameof(SelectedLCTF))]
-        public int WavelengthMax
-        {
-            get
-            {
-                return SelectedLCTF == null ? 0 : SelectedLCTF.LCTFDevice.WavelengthMax;
-            }
-        }
+		[DependsOn(nameof(SelectedLCTF))]
+		public int WavelengthMax
+		{
+			get
+			{
+				return SelectedLCTF == null ? 0 : SelectedLCTF.LCTFDevice.WavelengthMax;
+			}
+		}
 
-        [DependsOn(nameof(SelectedLCTF))]
-        public int WavelengthStep
-        {
-            get
-            {
-                return SelectedLCTF == null ? 0 : SelectedLCTF.LCTFDevice.WavelengthStep;
-            }
-        }
+		[DependsOn(nameof(SelectedLCTF))]
+		public int WavelengthStep
+		{
+			get
+			{
+				return SelectedLCTF == null ? 0 : SelectedLCTF.LCTFDevice.WavelengthStep;
+			}
+		}
 
-        [DependsOn(nameof(SelectedLCTF))]
-        public bool CanOperate
-        {
-            get
-            {
-                return SelectedLCTF == null ? false : SelectedLCTF.CurrentState == LCTFState.Ready;
-            }
-        }
+		[DependsOn(nameof(SelectedLCTF))]
+		public bool CanOperate
+		{
+			get
+			{
+				return SelectedLCTF == null ? false : SelectedLCTF.CurrentState == LCTFState.Ready;
+			}
+		}
 
-        public bool SequenceOrdered { get; set; } = true;
-        public bool SequenceArbitrary { get; set; } = false;
+		public bool SequenceOrdered { get; set; } = true;
+		public bool SequenceArbitrary { get; set; } = false;
 
-        public bool IsSequencing { get; set; } = false;
-        
-        [DependsOn(nameof(IsSequencing))]
-        public bool IsNotSequencing => !IsSequencing;
+		public bool IsSequencing { get; set; } = false;
 
-        public ObservableCollection<ArbitrarySequenceItem> ArbitrarySequenceItems {get;} = new ObservableCollection<ArbitrarySequenceItem>();
+		[DependsOn(nameof(IsSequencing))]
+		public bool IsNotSequencing => !IsSequencing;
 
-        public int OrderedSequenceStart { get; set; } = 0;
-        public int OrderedSequenceStop { get; set; } = 0;
-        public int OrderedSequenceStep { get; set; } = 1;
-        public int OrderedSequenceDwell { get; set; } = 30;
+		private CancellationTokenSource sequenceTokenSource;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+		public ObservableCollection<ArbitrarySequenceItem> ArbitrarySequenceItems { get; } = new ObservableCollection<ArbitrarySequenceItem>();
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            ArbitrarySequenceItems.Add(new ArbitrarySequenceItem() { Wavelength=800, DwellTime=50 });
-            DataContext = this;
+		public int OrderedSequenceStart { get; set; } = 0;
+		public int OrderedSequenceStop { get; set; } = 0;
+		public int OrderedSequenceStep { get; set; } = 1;
+		public int OrderedSequenceDwell { get; set; } = 30;
 
-            LCTFController.OnMcfAttached += Controller_OnMcfAttachedOrDetached;
-            LCTFController.OnMcfDetached += Controller_OnMcfAttachedOrDetached;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-            this.Controller_OnMcfAttachedOrDetached();
-        }
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			ArbitrarySequenceItems.Add(new ArbitrarySequenceItem() { Wavelength = 800, DwellTime = 50 });
+			DataContext = this;
 
-        private void Controller_OnMcfAttachedOrDetached()
-        {
-            if (SelectedLCTF != null && !LCTFController.AttachedLCTFs.Select(x => x.InstanceId).Contains(SelectedLCTF.LCTFDevice.InstanceId))
-            {
-                SelectedLCTF = null;
-            }
+			LCTFController.OnMcfAttached += Controller_OnMcfAttachedOrDetached;
+			LCTFController.OnMcfDetached += Controller_OnMcfAttachedOrDetached;
 
-            foreach (LCTFDevice lctf in LCTFController.AttachedLCTFs)
-            {
-                if (SelectedLCTF == null)
-                {
-                    SelectedLCTF = new LCTFDeviceModel(lctf);
+			this.Controller_OnMcfAttachedOrDetached();
+		}
 
-                    OrderedSequenceStart = WavelengthMin;
-                    OrderedSequenceStop = WavelengthMax;
-                    OrderedSequenceStep = WavelengthStep;
-                }
-            }
-        }
+		private void Controller_OnMcfAttachedOrDetached()
+		{
+			if (SelectedLCTF != null && !LCTFController.AttachedLCTFs.Select(x => x.InstanceId).Contains(SelectedLCTF.LCTFDevice.InstanceId))
+			{
+				SelectedLCTF = null;
+			}
 
-        private void HelpMenu_Click(object sender, RoutedEventArgs e)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var aboutWindow = new AboutWindow(this, assembly);
-            aboutWindow.ShowDialog();
-        }
+			foreach (LCTFDevice lctf in LCTFController.AttachedLCTFs)
+			{
+				if (SelectedLCTF == null)
+				{
+					SelectedLCTF = new LCTFDeviceModel(lctf);
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            SelectedLCTF?.Dispose();
-        }
+					OrderedSequenceStart = WavelengthMin;
+					OrderedSequenceStop = WavelengthMax;
+					OrderedSequenceStep = WavelengthStep;
+				}
+			}
+		}
 
-        private async void SequenceBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!IsSequencing)
-            {
-                IsSequencing = true;
-            }
-            else
-            {
-                return;
-            }
+		private void HelpMenu_Click(object sender, RoutedEventArgs e)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var aboutWindow = new AboutWindow(this, assembly);
+			aboutWindow.ShowDialog();
+		}
 
-            if (SequenceArbitrary)
-            {
-                var sequenceItems = ArbitrarySequenceItems.ToList();
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			SelectedLCTF?.Dispose();
+		}
 
-                Task sequenceTask = Task.Factory.StartNew(new Action(async () =>
-                {
-                    foreach (var item in sequenceItems)
-                    {
-                        if (item.Wavelength < SelectedLCTF.LCTFDevice.WavelengthMin || item.Wavelength > SelectedLCTF.LCTFDevice.WavelengthMax)
-                        {
-                            MessageBox.Show("Wavelength is outside the min and max of the LCTF.");
-                            IsSequencing = false;
-                            return;
-                        }
+		private async void SequenceBtn_Click(object sender, RoutedEventArgs e)
+		{
+			if (!IsSequencing)
+			{
+				IsSequencing = true;
+				sequenceTokenSource = new CancellationTokenSource();
+			}
+			else
+			{
+				sequenceTokenSource?.Cancel();
+				return;
+			}
 
-                        await SelectedLCTF.LCTFDevice.SetWavelengthAsync(item.Wavelength);
-                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentWavelength)));
+			var cancellationToken = sequenceTokenSource.Token;
+			Task sequenceTask = null;
 
-                        await Task.Delay(item.DwellTime);
-                    }
-                }));
+			if (SequenceArbitrary)
+			{
+				var sequenceItems = ArbitrarySequenceItems.ToList();
 
-                await sequenceTask;
-            }
-            else if (SequenceOrdered)
-            {
-                var wlStart = OrderedSequenceStart;
-                var wlStop = OrderedSequenceStop;
-                var wlStep = OrderedSequenceStep;
-                var wlDwell = OrderedSequenceDwell;
+				sequenceTask = Task.Factory.StartNew(new Action(async () =>
+				{
+					foreach (var item in sequenceItems)
+					{
+						cancellationToken.ThrowIfCancellationRequested();
 
-                if (wlStep == 0)
-                {
-                    MessageBox.Show("Cannot sequence with zero step size, sequence would never terminate.");
-                    IsSequencing = false;
-                    return;
-                }
-                
-                if ((wlStart < wlStop && wlStep < 0) || (wlStart > wlStop && wlStep > 0))
-                {
-                    MessageBox.Show("Start, stop, and step must be selected so that the sequence will terminate.");
-                    IsSequencing = false;
-                    return;
-                }
+						if (item.Wavelength < SelectedLCTF.LCTFDevice.WavelengthMin || item.Wavelength > SelectedLCTF.LCTFDevice.WavelengthMax)
+						{
+							MessageBox.Show("Wavelength is outside the min and max of the LCTF.");
+							IsSequencing = false;
+							return;
+						}
 
-                Task sequenceTask = Task.Run(async () =>
-                {
-                    for (var wlCurrent = wlStart; wlStep > 0 ? wlCurrent <= wlStop : wlCurrent >= wlStop; wlCurrent += wlStep)
-                    {
-                        await SelectedLCTF.LCTFDevice.SetWavelengthAsync(wlCurrent);
-                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentWavelength)));
+						await SelectedLCTF.LCTFDevice.SetWavelengthAsync(item.Wavelength);
+						PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentWavelength)));
 
-                        await Task.Delay(wlDwell);
-                    }
-                });
+						await Task.Delay(item.DwellTime, cancellationToken);
+					}
+				}), cancellationToken);
+			}
+			else if (SequenceOrdered)
+			{
+				var wlStart = OrderedSequenceStart;
+				var wlStop = OrderedSequenceStop;
+				var wlStep = OrderedSequenceStep;
+				var wlDwell = OrderedSequenceDwell;
 
-                await sequenceTask;
-            }
-            else
-            {
-                MessageBox.Show("Arbitrary or ordered sequencing must be selected.");
-                IsSequencing = false;
-                return;
-            }
+				if (wlStep == 0)
+				{
+					MessageBox.Show("Cannot sequence with zero step size, sequence would never terminate.");
+					IsSequencing = false;
+					return;
+				}
 
-            IsSequencing = false;
-        }
+				if ((wlStart < wlStop && wlStep < 0) || (wlStart > wlStop && wlStep > 0))
+				{
+					MessageBox.Show("Start, stop, and step must be selected so that the sequence will terminate.");
+					IsSequencing = false;
+					return;
+				}
 
-        private void MaskNumericInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !TextIsNumeric(e.Text);
-        }
+				sequenceTask = Task.Run(async () =>
+				{
+					for (var wlCurrent = wlStart; wlStep > 0 ? wlCurrent <= wlStop : wlCurrent >= wlStop; wlCurrent += wlStep)
+					{
+						cancellationToken.ThrowIfCancellationRequested();
 
-        private void MaskNumericPaste(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(string)))
-            {
-                string input = (string)e.DataObject.GetData(typeof(string));
-                if (!TextIsNumeric(input)) e.CancelCommand();
-            }
-            else
-            {
-                e.CancelCommand();
-            }
-        }
+						await SelectedLCTF.LCTFDevice.SetWavelengthAsync(wlCurrent);
+						PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentWavelength)));
 
-        private bool TextIsNumeric(string input)
-        {
-            return input.All(c => Char.IsDigit(c) || Char.IsControl(c));
-        }
+						await Task.Delay(wlDwell, cancellationToken);
+					}
+				}, cancellationToken);
+			}
+
+			try
+			{
+				await sequenceTask;
+			}
+			catch (OperationCanceledException)
+			{
+				;
+			}
+			finally
+			{
+				sequenceTokenSource.Dispose();
+			}
+
+			IsSequencing = false;
+		}
 
 #pragma warning disable CS0067 // Used by generated code
-        public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 #pragma warning restore CS0067
-    }
+	}
 }
